f6d5440609dca99525017b81388925b7ce1dce30
Julie-Des <julie.deshayes14@gmail.com>
2025-10-08 15:42:11 +0200

    feat: add cards in json files

---
+++ src/data/projectsData.ts
@@ -0,0 +1,14 @@
+export const projectsData = [
+  {
+    id: 0,
+    image: "/images/portfolio-cover.png",
+    githubLink: "https://github.com/Julie-Des/portfolio",
+    demoLink: "https://julie-deshayes.dev",
+  },
+  {
+    id: 1,
+    image: "/images/qwenta-cover.png",
+    githubLink: "https://github.com/Julie-Des/portfolio",
+    demoLink: "https://julie-deshayes.dev",
+  },
+];
